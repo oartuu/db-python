4cea1fc2f75ab481fec6f38b92a5d94efdb955c8
Arthur <artu.donascimento@gmail.com>
2024-04-22 15:48:32 -0300

    simi final

--- index.py
+++ index.py
@@ -4,11 +4,18 @@ import psycopg2
 app = Flask(__name__)
 
 # Dados de conexão ao banco de dados
-dbname = "rpg"
-user = "nome_do_user"
-password = "senha"
+dbname = "teste"
+user = "postgres"
+password = "sport1987"
 host = "localhost"
 port = "5432"
+
+# Consultas para cada etapa do jogo
+consultas_etapas = [
+    "SELECT * FROM combatentes WHERE habilidade = 'Velocidade';",
+    "SELECT * FROM combatentes WHERE habilidade = 'Camuflagem';",
+    "SELECT * FROM combatentes WHERE habilidade = 'Força';"
+]
 
 def conectar_banco():
     return psycopg2.connect(
@@ -71,7 +78,7 @@ def login():
 
             if usuario:
                 # Usuário autenticado, redirecionar para a página de sucesso
-                return redirect(url_for('combatentes'))
+                return redirect(url_for('etapa', numero_etapa=1))
             else:
                 # Usuário não autenticado, redirecionar de volta para a página de login
                 return render_template('login.html', mensagem="Credenciais inválidas. Tente novamente.")
@@ -85,6 +92,38 @@ def login():
                 conn.close()
 
     return render_template('login.html')
+
+@app.route('/etapa/<int:numero_etapa>', methods=['GET', 'POST'])
+def etapa(numero_etapa):
+    if request.method == 'POST':
+        consulta_usuario = request.form['consulta']
+        consulta_correta = consultas_etapas[numero_etapa - 1]  # Índice começa em 0
+
+        if consulta_usuario.strip().lower() == consulta_correta.lower():
+            try:
+                # Estabelecer conexão com o banco de dados
+                conn = conectar_banco()
+
+                # Criar um cursor para executar operações no banco de dados
+                cursor = conn.cursor()
+
+                # Executa a consulta no banco de dados
+                cursor.execute(consultas_etapas[numero_etapa - 1])  # Substitui a consulta dinâmica
+                resultados = cursor.fetchall()
+
+                proxima_etapa = numero_etapa + 1 if numero_etapa < len(consultas_etapas) else None
+
+                return render_template('resultado.html', resultados=resultados, etapa=numero_etapa, proxima_etapa=proxima_etapa)
+            except psycopg2.Error as e:
+                return f"Erro ao conectar ao banco de dados: {e}"
+            finally:
+                # Fechar conexão
+                if conn is not None:
+                    conn.close()
+        else:
+            return render_template('etapa.html', etapa=numero_etapa, erro=True)
+
+    return render_template('etapa.html', etapa=numero_etapa)
 
 @app.route('/combatentes')
 def combatentes():
@@ -111,3 +150,4 @@ def combatentes():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    app.run(host='0.0.0.0')
